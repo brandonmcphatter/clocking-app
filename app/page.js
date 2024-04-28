0ba0d1a1d98d86916b6d8f8a3212e8c0ae1100b8
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-04-28 17:49:24 -0500

    Deleted unused imports

--- app/page.js
+++ app/page.js
@@ -1,9 +1,4 @@
-import Image from "next/image";
-import ClockLogo from '/public/clocking-logo.png'
-import styles from "./page.module.css";
-import Link from "next/link";
-import Navbar from "@/UI/NavBar";
-
+import LoginBox from "@/UI/LoginBox";
 
 export default function Home() {
     return (
@@ -16,29 +11,3 @@ export default function Home() {
     )
         ;
 }
-
-function LoginBox() {
-    return (
-        <div className={'container border border-black my-5 w-50'}>
-            <form action="" className={'d-flex justify-content-center align-items-center flex-column row'}>
-                {/*email*/}
-                <div className={'d-flex justify-content-center'}>
-                    <label htmlFor="emailInput" className={''}>email:</label>
-                    <input type="text"/>
-                </div>
-
-                {/*password*/}
-                <div className={'d-flex justify-content-center'}>
-                    <label htmlFor="passwordInput">password:</label>
-                    <input type="password"/>
-                </div>
-
-                <div className={'d-flex justify-content-center'}>
-                    <button>LOGIN</button>
-                </div>
-            </form>
-        </div>
-    )
-}
-
-
