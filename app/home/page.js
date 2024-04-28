6f7ac94e0af5a3db92feb7fd7637e8f51c01308e
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-04-28 17:49:10 -0500

    Signed in home page

--- app/home/page.js
+++ app/home/page.js
@@ -1,8 +1,44 @@
+'use client';
+
 import React from 'react';
 
 function HomePage() {
+
+    function showTimeIn(){
+        const date = new Date();
+        const time = date.toLocaleTimeString();
+        alert('Punched In At ' + time);
+    }
+    function showTimeOut(){
+        const date = new Date();
+        const time = date.toLocaleTimeString();
+        alert('Punched Out At ' + time);
+    }
+
+
     return (
-        <div>Home Page After Login</div>
+        <div className="container">
+            {/*hours worked - pay period - prev pay period section*/}
+            <div className="container">
+
+            </div>
+
+            {/*clock in button */}
+            <div className="container">
+                <button onClick={showTimeIn}>Punch In</button>
+            </div>
+
+            {/*clock out button */}
+            <div className="container">
+                <button onClick={showTimeOut}>Punch Out</button>
+            </div>
+
+            {/*Mon - Fri details section*/}
+            <div className="container">
+
+            </div>
+        </div>
+
     );
 }
 
