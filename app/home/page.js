01fdd2a18d2997b3d3e6c050226843126468c050
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-04-29 00:08:42 -0500

    adding time section

--- app/home/page.js
+++ app/home/page.js
@@ -23,19 +23,34 @@ function HomePage() {
 
             </div>
 
-            {/*clock in button */}
-            <div className="container">
-                <button onClick={showTimeIn}>Punch In</button>
-            </div>
+            {/*top container for clock in - clock out functionality*/}
+            <div className={'container d-flex justify-content-center gap-3'}>
 
-            {/*clock out button */}
-            <div className="container">
-                <button onClick={showTimeOut}>Punch Out</button>
+                {/*clock in button */}
+                <div className="container bg-primary w-50">
+                    <button onClick={showTimeIn}>Punch In</button>
+                </div>
+
+                {/*clock out button */}
+                <div className="container bg-danger w-50">
+                    <button onClick={showTimeOut}>Punch Out</button>
+                </div>
             </div>
 
             {/*Mon - Fri details section*/}
-            <div className="container">
-
+            <div className="container border border-primary border-3">
+                <div className={'d-flex justify-content-between row text-decoration-underline'}>
+                    <div className={'col-md-3'}>Date</div>
+                    <div className={'col-md-3'}>Time In</div>
+                    <div className={'col-md-3'}>Time Out</div>
+                    <div className={'col-md-3'}>Total Hours</div>
+                </div>
+                <div className={'d-flex justify-content-between row'}>
+                    <div className={'col-md-3'}>7/4/24</div>
+                    <div className={'col-md-3'}>08:00am</div>
+                    <div className={'col-md-3'}>05:00pm</div>
+                    <div className={'col-md-3'}>9.0</div>
+                </div>
             </div>
         </div>
 
