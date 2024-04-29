c5824bd492e5039a3c470965b30dd80c6eda8f9b
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-04-29 17:11:24 -0500

    base layout for account page

--- app/account/page.js
+++ app/account/page.js
@@ -1,9 +1,86 @@
-
 function AccountPage() {
     return (
-     <div>
-         <h1>Account</h1>
-     </div>
+        <>
+        {/*Personal Details*/}
+        <div className={'container mb-5 bg-success'}>
+            <h1 className={'mb-3'}>Personal</h1>
+            <div className="row">
+                <div className={'col-md-6'}>
+                    <h3>First Name</h3>
+                </div>
+                <div className={'col-md-6'}>
+                    <h3>Last Name</h3>
+                </div>
+            </div>
+            <div className="row">
+                <div className={'col-md-6'}>
+                    <input type="text" id={'firstName'} style={{width: '90%'}}/>
+                </div>
+                <div className={'col-md-6'}>
+                    <input type="text" id={'lastName'} style={{width: '90%'}}/>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className={'col-md-6'}>
+                    <h3>Email</h3>
+                </div>
+                <div className={'col-md-6'}>
+                    <h3>Phone</h3>
+                </div>
+            </div>
+            <div className="row">
+                <div className={'col-md-6'}>
+                    <input type="email" id={'email'} style={{width: '90%'}}/>
+                </div>
+                <div className={'col-md-6'}>
+                    <input type="tel" id={'phone'} style={{width: '90%'}}/>
+                </div>
+            </div>
+        </div>
+            {/*Work Details*/}
+            <div className="container bg-warning-subtle mb-4">
+                <h1 className={'mb-3'}>Work</h1>
+
+                <div className="row">
+                    <div className={'col-md-6'}>
+                        <h3>Company</h3>
+                    </div>
+                    <div className={'col-md-6'}>
+                        <h3>Position</h3>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className={'col-md-6'}>
+                        <input type="text" id={'company'} style={{width: '90%'}}/>
+                    </div>
+                    <div className={'col-md-6'}>
+                        <input type="text" id={'position'} style={{width: '90%'}}/>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className={'col-md-6'}>
+                        <h3>Hire Date</h3>
+                    </div>
+                    <div className={'col-md-6'}>
+                        <h3>Hourly Wage</h3>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className={'col-md-6'}>
+                        <input type="text" id={'hireDate'} style={{width: '90%'}}/>
+                    </div>
+                    <div className={'col-md-6'}>
+                        <input type="text" id={'hourlyWage'} style={{width: '90%'}}/>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container d-flex justify-content-end">
+                <button>Save</button>
+            </div>
+        </>
     )
 }
 
