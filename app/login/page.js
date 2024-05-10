96128085ec71cc493b7814301eb7ece194f27ed6
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-05-10 00:53:51 -0500

    signup/login pages formatted

---
+++ app/login/page.js
@@ -0,0 +1,7 @@
+import LoginSection from "@/components/login-signup/LoginSection";
+
+export default function LoginPage() {
+    return (
+                <LoginSection/>
+    );
+}
---
+++ app/signup/page.js
@@ -0,0 +1,7 @@
+import SignupSection from "@/components/login-signup/SignupSection";
+
+export default function SignupPage() {
+    return (
+            <SignupSection />
+    );
+}
