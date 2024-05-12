ee85be560af7d08705e7e3cb3fb89f79560433f4
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-05-12 15:07:05 -0500

    responsive (hidden on mobile) / useEffect works properly

--- components/dashboard/WorkClock.js
+++ components/dashboard/WorkClock.js
@@ -1,20 +1,24 @@
+'use client'
 import React, {useEffect, useState} from "react";
 
 export default function WorkClock() {
     const [currentTime, setCurrentTime] = useState(new Date());
-    const tick = ()=> {setCurrentTime(new Date());}
     const clock = currentTime.toLocaleTimeString();
 
 
     // update time per second
     useEffect(() => {
-        tick();
+    const tick = ()=> {setCurrentTime(new Date());}
+        const timeoutId = setTimeout(() => {
+            tick();
+        }, 1000);
+        return () => clearTimeout(timeoutId);
     }, [currentTime]);
 
 
     return (
-        <div className={'w-1/2 text-8xl h-48 bg-teal-300  border-2 border-teal-800 flex text-center rounded-lg'}>
-            <div className={'w-full my-auto '}>
+        <div className={'hidden md:inline-block md:w-1/2 md:text-6xl lg:text-7xl bg-teal-300  border-2 border-teal-800  text-center rounded-lg grid place-content-center'}>
+            <div className={'w-full'}>
                 <span>{clock.length === 10 ? '0' + clock.substring(0, 7) : clock.substring(0, 8)}</span>
                 <span className={'text-2xl'}>{clock.length === 10 ? ' (' + clock.substring(8, clock.length) + ')' : clock.substring(9, clock.length)}</span>
             </div>
