5f790baa8b931429f30365c1e8d6c6304371e3ab
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-05-12 11:33:31 -0500

    background color change

--- app/layout.js
+++ app/layout.js
@@ -15,9 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <body className={inter.className}>
+    <body className={inter.className} style={{backgroundColor: '#FFF8F0'}}>
     <Navbar/>
-    <main>
+    <main className={'min-h-svh'} >
     {children}
     </main>
     </body>
