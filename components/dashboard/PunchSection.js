2dfcbaddd5fa4f1e948b409ca1753fed84bd74cd
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-05-12 15:06:28 -0500

    mobile responsive

--- components/dashboard/PunchSection.js
+++ components/dashboard/PunchSection.js
@@ -1,4 +1,4 @@
-'use client';
+
 
 import WorkClock from "@/components/dashboard/WorkClock";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
@@ -7,23 +7,23 @@ import React from "react";
 
 export default function PunchSection() {
     return (
-        <section className={'my-5'}>
+        <section className={'my-5 mx-2'}>
             <h2 className={'font-extrabold text-center mb-3'}>Clock In / Out</h2>
-            <div className={'w-full flex gap-1 h-48'}>
-                <PunchButton title={'Clock In'} color={'bg-green-800'}/>
+            <div className={'w-full flex gap-1'}>
+                <PunchButton title={'Clock In'} buttonColor={'bg-green-800'} iconColor={'yellow'}/>
                 <WorkClock/>
-                <PunchButton title={'Clock Out'} color={'bg-blue-500'}/>
+                <PunchButton title={'Clock Out'} buttonColor={'bg-blue-500'} iconColor={'red'}/>
             </div>
         </section>
     )
 }
 
-function PunchButton({title, color}) {
+function PunchButton({title, buttonColor, iconColor}) {
     return (
-        <div className={'w-1/4 h-full'}>
-            <button className={`${color} w-full h-full rounded-lg`}>
-                <FontAwesomeIcon icon={faBusinessTime} color={'red'} size={'4x'}/>
-                <div className={'text-2xl text-white italic font-extrabold'}>{title}</div>
+        <div className={'w-1/2 md:w-1/4 h-full'}>
+            <button className={`${buttonColor} w-full h-full rounded-lg py-2`}>
+                <FontAwesomeIcon icon={faBusinessTime} color={iconColor} size={'4x'}/>
+                <div className={'text-2xl text-white italic font-bold'}>{title}</div>
             </button>
         </div>
     )
