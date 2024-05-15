3d8b1f3219f12aec754f2812e5b1f91b1a6ba356
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-05-14 22:10:43 -0500

    section components for account page

---
+++ components/account/AccountEmailDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function AccountEmailDetail({name, label, value}) {
+    return (
+        <div className="mb-4 w-full md:pr-5">
+            <label htmlFor={name} className="block text-gray-600">{label}</label>
+            <div className=" w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 bg-transparent">
+                {value}
+            </div>
+        </div>
+    )
+}
---
+++ components/account/AccountPayDetail.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function AccountPayDetail({name, label, value}) {
+    return (
+        <div className="mb-4 w-full md:w-1/2 md:pr-5">
+            <label htmlFor={name} className="block text-gray-600">{label}</label>
+            <div
+                className=" w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 bg-transparent">
+                ${value.toFixed(2)}
+            </div>
+        </div>
+    )
+}
