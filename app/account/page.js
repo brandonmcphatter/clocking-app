e56382fa1f715c0f76b0183e3b0c4d740d2b3a83
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-05-14 22:15:20 -0500

    page format for account page complete

--- app/account/page.js
+++ app/account/page.js
@@ -1,87 +1,52 @@
-function AccountPage() {
+import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPencil} from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+import AccountPayDetail from "@/components/account/AccountPayDetail";
+import AccountDetail from "@/components/account/AccountDetail";
+import AccountEmailDetail from "@/components/account/AccountEmailDetail";
+
+
+export default function AccountPage() {
     return (
         <>
-        {/*Personal Details*/}
-        <div className={'container mb-5 bg-success'}>
-            <h1 className={'mb-3'}>Personal</h1>
-            <div className="row">
-                <div className={'col-md-6'}>
-                    <h3>First Name</h3>
-                </div>
-                <div className={'col-md-6'}>
-                    <h3>Last Name</h3>
-                </div>
-            </div>
-            <div className="row">
-                <div className={'col-md-6'}>
-                    <input type="text" id={'firstName'} style={{width: '90%'}}/>
-                </div>
-                <div className={'col-md-6'}>
-                    <input type="text" id={'lastName'} style={{width: '90%'}}/>
-                </div>
-            </div>
+            <div className="lg:p-36 md:p-24 sm:20 p-8 w-full">
+                <h1 className=" text-center text-3xl font-semibold mb-4">Account</h1>
+                <div>
+                    <h2 className={'text-2xl font-semibold mb-4'}>Personal:</h2>
+                    <div className={'w-full flex flex-wrap'}>
+                        {/*First Name Input*/}
+                        <AccountDetail label={'First Name'} name={'firstName'} value={'William'}/>
+                        {/*Last Name Input*/}
+                        <AccountDetail label={'Last Name'} name={'lastName'} value={'Richardson'}/>
+                        {/*Email Input*/}
+                        <AccountEmailDetail label={'Email'} name={'email'} value={'willrichardson1@gmail.com'}/>
 
-            <div className="row">
-                <div className={'col-md-6'}>
-                    <h3>Email</h3>
-                </div>
-                <div className={'col-md-6'}>
-                    <h3>Phone</h3>
-                </div>
-            </div>
-            <div className="row">
-                <div className={'col-md-6'}>
-                    <input type="email" id={'email'} style={{width: '90%'}}/>
-                </div>
-                <div className={'col-md-6'}>
-                    <input type="tel" id={'phone'} style={{width: '90%'}}/>
-                </div>
-            </div>
-        </div>
-            {/*Work Details*/}
-            <div className="container bg-warning-subtle mb-4">
-                <h1 className={'mb-3'}>Work</h1>
+                        {/*Edit Button */}
+                    <Link href={'/account/edit'} className={'w-full'}>
+                        <button type="submit" className="bg-gray-500 hover:bg-green-600 text-white font-semibold rounded-md py-2 px-4 w-full mt-3 text-xl">
+                            <span className={'mr-3'}>Edit</span>
+                            <span><FontAwesomeIcon icon={faPencil} /></span>
+                        </button>
+                    </Link>
+                    </div>
 
-                <div className="row">
-                    <div className={'col-md-6'}>
-                        <h3>Company</h3>
-                    </div>
-                    <div className={'col-md-6'}>
-                        <h3>Position</h3>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className={'col-md-6'}>
-                        <input type="text" id={'company'} style={{width: '90%'}}/>
-                    </div>
-                    <div className={'col-md-6'}>
-                        <input type="text" id={'position'} style={{width: '90%'}}/>
-                    </div>
-                </div>
+                    <hr className={'mt-5 mb-3'}/>
 
-                <div className="row">
-                    <div className={'col-md-6'}>
-                        <h3>Hire Date</h3>
+                    <h2 className={'text-2xl font-semibold mb-4'}>Work:</h2>
+                    <div className={'w-full flex flex-wrap'}>
+                        {/*Company Input*/}
+                        <AccountDetail label={'Company'} name={'company'} value={'Wal-Mart'}/>
+                        {/*Position Input*/}
+                        <AccountDetail label={'Position'} name={'position'} value={'Cashier'}/>
+                        {/*Pay Input*/}
+                        <AccountPayDetail label={'Pay Rate'} name={'pay'} value={3} readOnly={true} />
+                        {/*Hire Date Input*/}
+                        <AccountDetail label={'Hire Date'} name={'hireDate'} type={'date'} value={'03/01/2024'}/>
                     </div>
-                    <div className={'col-md-6'}>
-                        <h3>Hourly Wage</h3>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className={'col-md-6'}>
-                        <input type="text" id={'hireDate'} style={{width: '90%'}}/>
-                    </div>
-                    <div className={'col-md-6'}>
-                        <input type="text" id={'hourlyWage'} style={{width: '90%'}}/>
-                    </div>
-                </div>
-            </div>
 
-            <div className="container d-flex justify-content-end">
-                <button>Save</button>
+                </div>
             </div>
         </>
     )
 }
-
-export default AccountPage;
