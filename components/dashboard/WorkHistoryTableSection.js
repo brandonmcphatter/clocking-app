e6f0780e0866ea0c46005318a500cec4a61c1bec
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-05-14 22:10:12 -0500

    side margin added

--- components/dashboard/WorkHistoryTableSection.js
+++ components/dashboard/WorkHistoryTableSection.js
@@ -4,7 +4,7 @@ import WorkHistoryTable from "@/components/dashboard/WorkHistoryTable";
 export default function WorkHistoryTableSection() {
     return (
 
-        <section className={''}>
+        <section className={'mx-2'}>
             <h2 className={'font-extrabold text-center mb-3'}>Work History</h2>
             <WorkHistoryTable />
         </section>
