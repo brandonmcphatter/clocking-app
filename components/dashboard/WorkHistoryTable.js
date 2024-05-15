e4262885dd21676b220ba2b5cae56b15f8050c47
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-05-14 22:10:00 -0500

    border added around table

--- components/dashboard/WorkHistoryTable.js
+++ components/dashboard/WorkHistoryTable.js
@@ -3,10 +3,12 @@ import React from "react";
 
 export default function WorkHistoryTable() {
     return (
-        <table className={'w-full table overflow-auto'}>
-            <WorkHistoryTableHeader />
-            <WorkHistoryTableBody />
-        </table>
+        <div className={'border-8 rounded-lg '} style={{backgroundColor: '#F4D06F' , borderColor: '#F4D06F'}}>
+            <table className={'w-full table overflow-auto '}>
+                <WorkHistoryTableHeader />
+                <WorkHistoryTableBody />
+            </table>
+        </div>
     )
 }
 
