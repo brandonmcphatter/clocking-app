602191b0badac640472d521e0bd7aa6dfc2f8471
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-05-18 21:00:48 -0500

    function added to add new employee to db

--- services/supabase.js
+++ services/supabase.js
@@ -1,4 +1,5 @@
 import {createClient} from '@supabase/supabase-js';
+import {redirect} from "next/navigation";
 
 const supabaseUrl = 'https://idwrhghteppqmagmlpmi.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlkd3JoZ2h0ZXBwcW1hZ21scG1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU3NDYyOTEsImV4cCI6MjAzMTMyMjI5MX0.F-0AtBSKW42urpfiE5HW36lJqBDHTWHghFRwuz2P1fc'
@@ -15,4 +16,26 @@ async function getEmployee(id) {
     return Employee[0];
 }
 
-export {supabase, getEmployee};
+async function addEmployee(newUser) {
+
+    const {data, error} = await supabase
+        .from('Employee')
+        .insert([
+            {
+                FirstName: newUser.firstName,
+                LastName: newUser.lastName,
+                Email: newUser.email
+            },
+        ])
+        .select()
+
+    if (error) {
+        console.log(error)
+        return
+    }
+
+    alert('New User Created')
+    redirect('/account/dashboard')
+}
+
+export {supabase, getEmployee, addEmployee};
