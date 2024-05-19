33c2345e861abbe2397f57df72621cab454b3431
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-05-18 20:59:10 -0500

    footer style finished

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,14 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div>
-
-        </div>
+        <footer className={'w-1/2 mx-auto mt-5 mb-3'}>
+            <hr/>
+            <div className={' flex justify-center mt-4'}>
+                <div className={'italic text-sm text-nowrap'}>
+                    The Clocking Company© 2024
+                </div>
+            </div>
+        </footer>
     );
 }
 
