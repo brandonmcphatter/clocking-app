b2c0fd669bd220e60c2dd8467eab77da7786865a
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-05-19 23:56:23 -0500

    Added extra attributes

--- components/login-signup/FormInput.js
+++ components/login-signup/FormInput.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-export default function FormInput({label, name, type}) {
+export default function FormInput({label, name, type, value, set}) {
     return (
         <div className="mb-4">
             <label htmlFor={name} className="block text-gray-600">{label}</label>
-            <input type={type} id={name} name={name}
-                   className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
-                   autoComplete="off"/>
+            <input type={type} id={name} name={name} value={value}
+                   className="w-full border border-gray-300
+                   rounded-md py-2 px-3 focus:outline-none
+                   focus:border-blue-500"
+                   autoComplete="off" onChange={e => set(e.target.value)}/>
         </div>
     )
 }
